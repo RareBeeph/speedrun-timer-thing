9606cf3934f9e74f1baee18f8bcda4c4fb344db0
Keiran Jensen <keiranjensen@gmail.com>
2023-09-07 18:24:49 -0400

    Add timer that increases

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"math"
+	"strconv"
+	"time"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -10,10 +16,49 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("Hello World")
 
-	w.SetContent(widget.NewLabel("Hello World!"))
+	// dark := color.NRGBA{R: 16, G: 16, B: 24, A: 255}
+	// w.Canvas().SetContent()
+
+	layout := layout.NewGridLayout(1)
+	timer := widget.NewLabel("00:00:00.000")
+	helloworld := widget.NewLabel("Hello World!")
+	content := container.New(layout, helloworld, timer)
+
+	w.SetContent(content)
 	w.Canvas().SetOnTypedKey(func(k *fyne.KeyEvent) {
-		w.Content().(*widget.Label).Text += string(k.Name)
-		w.Content().Refresh()
+		helloworld.Text += string(k.Name)
+		helloworld.Refresh()
 	})
+
+	startTime := time.Now()
+	duration := time.Since(startTime)
+	go func() {
+		for {
+			duration = time.Since(startTime)
+
+			// collect hh, mm, ss.sss for formatting
+			hours := zeroPad(strconv.Itoa((int)(math.Floor(duration.Hours()))), 2)
+			minutes := zeroPad(strconv.Itoa((int)(math.Floor(duration.Minutes()))%60), 2)
+			seconds := zeroPad(strconv.Itoa((int)(math.Floor(duration.Seconds()))%60), 2)
+			millis := zeroPad(strconv.Itoa((int)(duration.Milliseconds()%1000)), 3)
+
+			// usually format as mm:ss.sss
+			timer.Text = minutes + ":" + seconds + "." + millis
+			// but if hours are needed, format as hh:mm:ss.sss
+			if hours != "00" {
+				timer.Text = hours + ":" + timer.Text
+			}
+
+			timer.Refresh()
+		}
+	}()
+
 	w.ShowAndRun()
+}
+
+func zeroPad(in string, length int) string {
+	for len(in) < length {
+		in = "0" + in
+	}
+	return in
 }
