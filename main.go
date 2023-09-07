baebc2f1c11cbaf50a02d2bf3cb1f440c5260283
Keiran Jensen <keiranjensen@gmail.com>
2023-09-07 19:10:28 -0400

    Make timer startable, pausable, and resettable

--- main.go
+++ main.go
@@ -16,31 +16,67 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("Hello World")
 
-	// dark := color.NRGBA{R: 16, G: 16, B: 24, A: 255}
-	// w.Canvas().SetContent()
-
 	layout := layout.NewGridLayout(1)
 	timer := widget.NewLabel("00:00:00.000")
 	helloworld := widget.NewLabel("Hello World!")
 	content := container.New(layout, helloworld, timer)
 
+	timerIsRunning := false
+	var lastStart time.Time
+	// i should probably just store the total unpaused duration instead of this but i'll worry about that later
+	var pauseSegments []pauseSegment
+
 	w.SetContent(content)
 	w.Canvas().SetOnTypedKey(func(k *fyne.KeyEvent) {
 		helloworld.Text += string(k.Name)
 		helloworld.Refresh()
+
+		if k.Name == fyne.KeySpace {
+			if timerIsRunning {
+				pauseSegments = append(pauseSegments, pauseSegment{lastStart, time.Since(lastStart)})
+			} else {
+				lastStart = time.Now()
+			}
+			timerIsRunning = !timerIsRunning
+		}
+
+		if k.Name == fyne.KeyBackspace {
+			timerIsRunning = false
+			pauseSegments = []pauseSegment{}
+			timer.Text = "00:00.000"
+			timer.Refresh()
+		}
 	})
 
-	startTime := time.Now()
-	duration := time.Since(startTime)
+	var duration time.Duration
+	var hours, minutes, seconds, millis string
+	var scratchwork, reference time.Time
+
 	go func() {
+		// probably unideal to just have this shmoving as fast as the goroutine will let it
 		for {
-			duration = time.Since(startTime)
+			// dummy variables for the purpose of adding durations together
+			reference = time.Now()
+			scratchwork = time.Now()
+
+			if timerIsRunning {
+				scratchwork = scratchwork.Add(time.Since(lastStart))
+			} else if len(pauseSegments) == 0 {
+				// If the timer hasn't done anything since reset, skip calculating what it should say
+				continue
+			}
+
+			// add on the durations that the timer was running for between all pauses since reset
+			for _, segment := range pauseSegments {
+				scratchwork = scratchwork.Add(segment.dur)
+			}
+			duration = scratchwork.Sub(reference)
 
 			// collect hh, mm, ss.sss for formatting
-			hours := zeroPad(strconv.Itoa((int)(math.Floor(duration.Hours()))), 2)
-			minutes := zeroPad(strconv.Itoa((int)(math.Floor(duration.Minutes()))%60), 2)
-			seconds := zeroPad(strconv.Itoa((int)(math.Floor(duration.Seconds()))%60), 2)
-			millis := zeroPad(strconv.Itoa((int)(duration.Milliseconds()%1000)), 3)
+			hours = zeroPad(strconv.Itoa((int)(math.Floor(duration.Hours()))), 2)
+			minutes = zeroPad(strconv.Itoa((int)(math.Floor(duration.Minutes()))%60), 2)
+			seconds = zeroPad(strconv.Itoa((int)(math.Floor(duration.Seconds()))%60), 2)
+			millis = zeroPad(strconv.Itoa((int)(duration.Milliseconds()%1000)), 3)
 
 			// usually format as mm:ss.sss
 			timer.Text = minutes + ":" + seconds + "." + millis
@@ -61,4 +97,9 @@ func zeroPad(in string, length int) string {
 		in = "0" + in
 	}
 	return in
+}
+
+type pauseSegment struct {
+	start time.Time
+	dur   time.Duration
 }
