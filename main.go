12a57b51b10da61228ad12973cc740128319b3eb
Keiran Jensen <keiranjensen@gmail.com>
2023-09-21 19:25:24 -0400

    Handle UI as a table; add PB handling to SplitHandler

--- main.go
+++ main.go
@@ -27,39 +27,44 @@ func init() {
 }
 
 func main() {
-	var splitHandler = SplitHandler{Splits: []Split{
+	var (
+		app           = app.New()
+		window        = app.NewWindow("Timer Thing (placeholder)")
+		timer         = &Timer{}
+		timerText     = binding.NewString()
+		timerLabel    = widget.NewLabelWithData(timerText)
+		titleBarLabel = widget.NewLabel("Fake Game Title")
+	)
+
+	timerLabel.Alignment = fyne.TextAlignCenter
+	titleBarLabel.Alignment = fyne.TextAlignCenter
+
+	var splitHandler = &SplitHandler{Splits: []Split{
 		{Name: "Fake Split 1", TimeInPB: time.Duration(154500000000), BestSegment: time.Duration(153983000000)},
 		{Name: "Fake Split 2", TimeInPB: time.Duration(400000000000), BestSegment: time.Duration(398000000000)},
 	}}
 
-	var (
-		app            = app.New()
-		window         = app.NewWindow("Hello World")
-		layout1        = layout.NewGridLayout(1)
-		timerText      = binding.NewString()
-		timerLabel     = widget.NewLabelWithData(timerText)
-		testDiffLabel0 = widget.NewLabel("")
-		testDiffLabel1 = widget.NewLabel("")
-		testTimeLabel0 = widget.NewLabel(StringifyMilliseconds(splitHandler.Splits[0].TimeInPB.Milliseconds()))
-		testTimeLabel1 = widget.NewLabel(StringifyMilliseconds(splitHandler.Splits[1].TimeInPB.Milliseconds()))
-		titleBarLabel  = widget.NewLabel("Fake Game Title")
-		timer          = &Timer{}
-	)
+	var splitsTable = widget.NewTable(
+		func() (int, int) {
+			return len(splitHandler.Splits), 3
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("long enough content")
+		},
+		func(i widget.TableCellID, o fyne.CanvasObject) {
+			thisLabel := o.(*widget.Label)
+			if i.Col == 0 {
+				thisLabel.SetText(splitHandler.Splits[i.Row].Name)
+			} else if i.Col == 1 {
+				thisLabel.Alignment = fyne.TextAlignTrailing
+				thisLabel.SetText("-")
+			} else if i.Col == 2 {
+				thisLabel.Alignment = fyne.TextAlignTrailing
+				thisLabel.SetText(StringifyMilliseconds(splitHandler.GetTime(i.Row).Milliseconds()))
+			}
+		})
 
-	testDiffLabel0.Alignment = fyne.TextAlignTrailing
-	testDiffLabel1.Alignment = fyne.TextAlignTrailing
-	testTimeLabel0.Alignment = fyne.TextAlignTrailing
-	testTimeLabel1.Alignment = fyne.TextAlignTrailing
-	titleBarLabel.Alignment = fyne.TextAlignCenter
-	timerLabel.Alignment = fyne.TextAlignCenter
-
-	var (
-		splitLabel1 = container.New(layout.NewGridLayout(3), widget.NewLabel(splitHandler.Splits[0].Name), testDiffLabel0, testTimeLabel0)
-		splitLabel2 = container.New(layout.NewGridLayout(3), widget.NewLabel(splitHandler.Splits[1].Name), testDiffLabel1, testTimeLabel1)
-		splitLabels = container.New(layout1, splitLabel1, splitLabel2)
-		scrollable  = container.NewVScroll(splitLabels)
-		content     = container.New(layout1, titleBarLabel, scrollable, timerLabel)
-	)
+	var content = container.New(layout.NewGridLayout(1), titleBarLabel, splitsTable, timerLabel)
 
 	timerText.Set(timer.String())
 
@@ -82,17 +87,18 @@ func main() {
 			if timer.Stopped() {
 				timer.Restart()
 				splitHandler.Restart()
+				splitsTable.Refresh()
 			} else if timer.Running() || timer.Paused() {
 				timer.Stop()
 			}
 		}
 
 		if k.Name == fyne.KeyReturn {
-			if splitHandler.Index < len(splitLabels.Objects) {
-				splitLabels.Objects[splitHandler.Index].(*fyne.Container).Objects[2].(*widget.Label).Text = StringifyMilliseconds(timer.Milliseconds())
-				splitLabels.Objects[splitHandler.Index].(*fyne.Container).Objects[2].Refresh()
+			// TODO: make SplitHandler a proper state machine.
+			if timer.Milliseconds() != 0 {
+				splitHandler.Split(time.Duration(timer.Milliseconds() * 1000000))
 			}
-			splitHandler.Split(time.Duration(timer.Milliseconds() * 1000000))
+			splitsTable.Refresh()
 		}
 	})
 
@@ -111,6 +117,7 @@ func main() {
 	// but we want that size to be saved with the run data and not hardcoded
 	// window.SetFixedSize(true)
 	// window.Resize(fyne.NewSize(window.Canvas().Size().Width, 720))
+	window.Resize(fyne.NewSize(540, 300))
 
 	window.ShowAndRun()
 }
--- splithandler.go
+++ splithandler.go
@@ -15,9 +15,34 @@ func (h *SplitHandler) Split(time time.Duration) {
 	}
 
 	h.Splits[h.Index].TimeInActiveRun = time
+
+	// untested code
+	segmentTime := h.Splits[h.Index].TimeInActiveRun
+	if h.Index != 0 {
+		segmentTime -= h.Splits[h.Index-1].TimeInActiveRun
+	}
+	if segmentTime < h.Splits[h.Index].BestSegment {
+		h.Splits[h.Index].BestSegment = segmentTime
+	}
+
 	h.Index++
 }
 
 func (h *SplitHandler) Restart() {
+	for i := range h.Splits {
+		if h.Index >= len(h.Splits) && h.Splits[len(h.Splits)-1].TimeInActiveRun < h.Splits[len(h.Splits)-1].TimeInPB {
+			h.Splits[i].TimeInPB = h.Splits[i].TimeInActiveRun
+		}
+		h.Splits[i].TimeInActiveRun = time.Duration(0)
+	}
 	h.Index = 0
+}
+
+func (h *SplitHandler) GetTime(splitIdx int) time.Duration {
+	s := h.Splits[splitIdx]
+	if s.TimeInActiveRun.Milliseconds() == time.Duration(0).Milliseconds() {
+		return s.TimeInPB
+	} else {
+		return s.TimeInActiveRun
+	}
 }
