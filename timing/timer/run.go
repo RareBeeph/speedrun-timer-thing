daff6321af28fd0c00dcea5066d5b966f8f78083
Aria Taylor <ari@aricodes.net>
2024-02-15 15:40:05 -0800

    Add Run type

---
+++ timing/timer/run.go
@@ -0,0 +1,14 @@
+package timer
+
+import (
+	"speedruntimer/timing/splitter"
+)
+
+type Split = splitter.Split
+
+type Run struct {
+	GameName string
+	Category string
+	Segments []Split
+	Attempts int
+}
